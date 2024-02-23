6919df5debde6c210a500bb9e1e6fed337b98f8f
Ekaterina <katanatroll@yandex.ru>
2024-02-23 14:25:16 +0300

    add comments

--- main2.py
+++ main2.py
@@ -5,21 +5,25 @@ from sklearn.model_selection import train_test_split
 
 # Загрузка датасета Iris
 iris = load_iris()
-X = iris.data
-y = iris.target
+X = iris.data #Массив значений
+y = iris.target #Массив правильных ответов
 
 # Преобразование меток классов в бинарный формат
+# Было [0,...,1,..,2...], где каждая цифра обозначает вид ириса
+# Стало [[1,0,0], [0,1,0], [0,0,1]]
 def to_categorical(y):
     y_cat = np.zeros((len(y), 3))
     for i in range(len(y)):
         y_cat[i, y[i]] = 1
     return y_cat
 
+# Ниже два варианта нормализации значений в массиве данных ирисов(дают +- одинаковые значения)
+
 # Нормализация значений
 def normalized(x):
     return (x - np.mean(x, axis=0)) / np.std(x, axis=0)
 
-# Мин макс нормализация
+# Мин макс нормализация диапазон от 0 до 1
 def minMaxNormalized(x):
     return (x - np.min(x, axis=0))/(np.max(x, axis=0) - np.min(x, axis=0))
 
@@ -32,11 +36,12 @@ X_norm_train = normalized(X_train)
 X_norm_test = normalized(X_test)
 y_cat = to_categorical(y_train)
 
-input_dim = X.shape[1] #Кол-во входных данных
+input_dim = X.shape[1] #Кол-во входных данных (4)
 hidden_dim = 10 #Кол-во нейронов в скрытом слое
 out_dim = 3 #Кол-во выходных классов
 
 #Задаёт рандомные значения для весов и смещения первого входного слоя (Инициализация Ксавье)
+#Нужно для нормализации весов, все веса будут инициализироваться в диапазоне от 0 до 1
 stddev = np.sqrt(2.0 / (input_dim + out_dim))
 weights_in = np.random.normal(0, stddev, (input_dim, hidden_dim))
 bios_in = np.random.rand(hidden_dim)
@@ -48,7 +53,7 @@ bios_hid = np.random.rand(out_dim)
 def relu(vector): #функция активации (принимает вектор)
     return np.maximum(vector,0) #Максимум из пришедшего значения и нуля
 
-def relu_derivative(vector):
+def relu_derivative(vector): # Производная для функции активации 
     return np.where(vector> 0 , 1, 0)
 
 
@@ -59,8 +64,8 @@ def softmax(vector): #превращает произвольный вектор
 
 learning_rate = 0.001 #скорость обучения
 epochs = 400 #циклы(эпохи)
-errors=[]
-weights_in_changes =[]
+errors=[]# Массив ошибок для грфика
+weights_in_changes =[]# Массив весов для графика
 
 for epoch in range(epochs):
   
@@ -90,10 +95,10 @@ for epoch in range(epochs):
     hid_er = out_del @ weights_hid.T
     hid_del = hid_er * relu_derivative(input_ex)
 
-    #Обновление весов и смещений
+    #Обновление весов и смещений выходной слой
     weights_hid +=input_ex.T @ out_del * learning_rate
     bios_hid += np.sum(out_del, axis=0) * learning_rate
-
+    #Обновление весов и смещений входной слой
     weights_in+= X_norm_train.T @ hid_del * learning_rate
     if(epoch % 200 == 0):
         weights_in_changes.append(weights_in)
@@ -108,7 +113,7 @@ def predict(X):
     exits = softmax(vector_hid)
     
     return exits
-
+# Построение графика обучения
 def graphErr(arrErr, epochs):
     plt.plot(range(epochs), arrErr)
     plt.xlabel('Epochs')
@@ -117,7 +122,7 @@ def graphErr(arrErr, epochs):
 
 graphErr(errors, epochs)
 
-# Построение графика изменения весов
+# Построение графика изменения входных весов
 def graphWeightsChange(weightArr):
     plt.figure(figsize=(10, 5))
     for i, weights in enumerate(weightArr):
@@ -130,11 +135,13 @@ def graphWeightsChange(weightArr):
 
 graphWeightsChange(weights_in_changes)
 
+# Прогон по тренировочным значениям в среднем 96-98% точности
 pred_train = predict(X_norm_train)
 accuracy_train = (np.mean(np.argmax(pred_train, axis=1)==y_train))
 accuracy_rd_per_train = round(accuracy_train, 4) * 100
 print("Точность модели на обучающем наборе данных: ", accuracy_rd_per_train, "%")
 
+# Прогон по тестовым значениям, которые нейросеть ещё не видела в среднем 93-96% точности
 pred_test = predict(X_norm_test)
 accuracy_test = (np.mean(np.argmax(pred_test, axis=1)==y_test))
 accuracy_rd_per_test = round(accuracy_test, 4) * 100
